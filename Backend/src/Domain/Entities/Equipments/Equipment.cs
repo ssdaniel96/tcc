05f68ec565eb76d4859f03febba1674ca126dd31
IATec - Daniel da Silva Soares <daniel.soares@iatec.com>
2023-05-01 23:01:16 -0300

    Solution created

--- Backend/src/Domain/Entities/Equipments/Equipment.cs
+++ Backend/src/Domain/Entities/Equipments/Equipment.cs
@@ -1,22 +1,22 @@
-﻿namespace Domain.Entities.Equipments;
+﻿using Domain.Entities.Events;
+using Domain.Entities.People;
+
+namespace Domain.Entities.Equipments;
 
 public sealed class Equipment : Entity
 {
     public string RFTag { get; private set; }
     public string Description { get; private set; }
-    public int ResponsibleId { get; private set; }
-    
-    //navigations
     public Collaborator Responsible { get; private set; }
-    
+
+    //navigations
     public IReadOnlyList<Event> Events => _events.AsReadOnly();
-    private List<Event> _events = new List<Event>();
+    private readonly List<Event> _events = new();
 
     public Equipment(string rfTag, string description, Collaborator responsible)
     {
         RFTag = rfTag;
         Description = description;
         Responsible = responsible;
-        ResponsibleId = responsible.Id;
     }
 }
