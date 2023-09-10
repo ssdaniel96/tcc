704dc2f1eaf2afb5544eb6a88451c15fd49005e4
dsoares <daniel_soares2007@hotmail.com>
2023-09-09 23:02:14 -0300

    removed obselete files

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EquipamentosComponent } from './pages/equipamentos/equipamentos.component';
-import { EventosComponent } from './pages/eventos/eventos.component';
 import { HistoricoComponent } from './pages/historico/historico.component';
 import { LocalizacoesComponent } from './pages/localizacoes/localizacoes.component';
 import { SimulacaoComponent } from './pages/simulacao/simulacao.component';
 
 const routes: Routes = [
   {path: 'equipamentos', component: EquipamentosComponent },
-  {path: 'eventos', component: EventosComponent },
   {path: 'historico', component: HistoricoComponent },
   {path: 'localizacoes', component: LocalizacoesComponent },
   {path: 'simulacao', component: SimulacaoComponent },
--- FrontEnd/src/app/pages/pages.module.ts
+++ FrontEnd/src/app/pages/pages.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HistoricoComponent } from '../pages/historico/historico.component';
-import { EventosComponent } from '../pages/eventos/eventos.component';
 import { EquipamentosComponent } from '../pages/equipamentos/equipamentos.component';
 import { LocalizacoesComponent } from '../pages/localizacoes/localizacoes.component';
 import { SimulacaoComponent } from '../pages/simulacao/simulacao.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppModule } from '../app.module';
 import { FormsModule } from '@angular/forms';
 
 
@@ -14,7 +11,6 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     HistoricoComponent,
-    EventosComponent,
     EquipamentosComponent,
     LocalizacoesComponent,
     SimulacaoComponent
@@ -22,7 +18,6 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     FormsModule
-    // BrowserModule
   ]
 })
 export class PagesModule { }
